772855dcdcb63fc470d1ea09431ebc109be330d4
Pratik <sarangipratikcode@gmail.com>
2023-10-04 01:07:26 +0530

    Worked on the profile screen

--- lib/screens/part_screens/profile.dart
+++ lib/screens/part_screens/profile.dart
@@ -1,12 +1,51 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_widget_from_html/flutter_widget_from_html.dart';
 
 class ProfileScreen extends StatelessWidget {
   const ProfileScreen({super.key});
+  final documentId = "salon1@clipngo.com";
 
   @override
   Widget build(BuildContext context) {
-    return const Center(
-      child: Text("Profile"),
+    print("building profile screen");
+    return FutureBuilder<DocumentSnapshot>(
+      future: FirebaseFirestore.instance
+          .collection('email-salons')
+          .doc(documentId)
+          .get(),
+      builder: (context, snapshot) {
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          return const CircularProgressIndicator();
+        }
+
+        if (snapshot.hasError) {
+          return Text('Error: ${snapshot.error}');
+        }
+
+        if (!snapshot.hasData || !snapshot.data!.exists) {
+          return const Text('Document not found');
+        }
+
+        // Document data is available here
+        var documentData = snapshot.data!.data() as Map<String, dynamic>;
+        String name = documentData['name'];
+        String phoneNumber = documentData['number'];
+        String emailId = documentData['email'];
+        String address = documentData['address'];
+        String image = documentData['image'];
+
+        return Column(
+          children: [
+            Text('Name: $name'),
+            Text('Phone Number: $phoneNumber'),
+            Text('Email ID: $emailId'),
+            Text('Address: $address'),
+            HtmlWidget(
+                "<img src=$image alt=\"Description of the image\">"), // Display the image using its URL
+          ],
+        );
+      },
     );
   }
 }
--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,14 +5,26 @@
 import FlutterMacOS
 import Foundation
 
+import audio_session
 import cloud_firestore
 import firebase_auth
 import firebase_core
+import just_audio
+import package_info_plus
 import path_provider_foundation
+import sqflite
+import url_launcher_macos
+import wakelock_plus
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  AudioSessionPlugin.register(with: registry.registrar(forPlugin: "AudioSessionPlugin"))
   FLTFirebaseFirestorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseFirestorePlugin"))
   FLTFirebaseAuthPlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseAuthPlugin"))
   FLTFirebaseCorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseCorePlugin"))
+  JustAudioPlugin.register(with: registry.registrar(forPlugin: "JustAudioPlugin"))
+  FLTPackageInfoPlusPlugin.register(with: registry.registrar(forPlugin: "FLTPackageInfoPlusPlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
+  SqflitePlugin.register(with: registry.registrar(forPlugin: "SqflitePlugin"))
+  UrlLauncherPlugin.register(with: registry.registrar(forPlugin: "UrlLauncherPlugin"))
+  WakelockPlusMacosPlugin.register(with: registry.registrar(forPlugin: "WakelockPlusMacosPlugin"))
 }
